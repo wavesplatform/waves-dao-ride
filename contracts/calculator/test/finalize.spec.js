4f57cdcbf2624ba915d351310095046aecf2b69f
Waves Rider <108881461+ridev6@users.noreply.github.com>
2023-06-21 15:30:11 +0500

    finalize can invoke anyone

--- contracts/calculator/test/finalize.spec.js
+++ contracts/calculator/test/finalize.spec.js
@@ -26,28 +26,6 @@ describe(`[${process.pid}] calculator: finalize`, () => {
     }, accounts.user1.seed))
   })
 
-  it('only factory can call finalize', async () => {
-    const newTreasuryVolumeInWaves = 1000 * 1e8
-    const xtnPrice = 0.05 * 1e8
-    const pwrManagersBonus = 0.2 * 1e8
-    const treasuryVolumeDiffAllocationCoef = 0
-
-    return expect(broadcastAndWait(invokeScript({
-      dApp: accounts.factory.address,
-      call: {
-        function: 'finalize',
-        args: [
-          { type: 'integer', value: newTreasuryVolumeInWaves },
-          { type: 'integer', value: xtnPrice },
-          { type: 'integer', value: pwrManagersBonus },
-          { type: 'integer', value: treasuryVolumeDiffAllocationCoef }
-        ]
-      },
-      payment: [],
-      chainId
-    }, accounts.user1.seed))).to.be.rejectedWith('permission denied')
-  })
-
   it('period should be finalized', async () => {
     const paymentAmount = 1
     const newTreasuryVolumeInWaves = 1000 * 1e8
